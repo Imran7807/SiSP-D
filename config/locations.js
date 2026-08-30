b809606bfcba49f2c48c9c704f6be39c8059615c
mrwasif-dev <happyprincetemp@gmail.com>
2026-08-30 17:02:06 +0500

    Add files via upload

---
+++ config/locations.js
@@ -0,0 +1,25 @@
+// Coverage area list, Urdu names, alphabetically ordered (Urdu abjad order).
+// The last entry is always the "None of these / Other" option, which reveals
+// a free-text field on the signup page so the user can type their own area.
+const LOCATIONS = [
+  'بٹھہ باغ شاہ',
+  'بیر والا',
+  'ٹالہی والا',
+  'چبھہ چبکن',
+  'چھتی بستی',
+  'چھوٹی دھنوٹ',
+  'دولت شاہ والا',
+  'دھنوٹ',
+  'دھنوٹ سے لودھراں مین روڈ',
+  'سعید چوک',
+  'کالہے والا',
+  'گڑھے موڑ',
+  'لائیل پور',
+  'مڑہی موڑ',
+  'منشی والا پل',
+  'نالے والا'
+];
+
+const OTHER_LOCATION = 'ان میں سے کوئی نہیں';
+
+module.exports = { LOCATIONS, OTHER_LOCATION };
